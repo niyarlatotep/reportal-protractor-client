66a6905c15a34fbef7bca79155a789d71efe2c43
Elnar Khisamov <khisamov.elnar@infotecs.ru>
2019-06-03 09:59:49 +0300

    screenshots sending add

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
-import * as path from 'path';
-import {browser} from "protractor";
+import {browser, ProtractorBrowser} from "protractor";
 import axios from 'axios';
-//todo add typings
+import FormData from "form-data";
+import Axios from "axios";
 
+//todo add typings and move to helpers
 function replaceInvalidSymbols(path: string) {
     //replace symbols that forbidden as Windows folder name
     return path.replace(/[\s\\/:*?"<>|.#]/g, '_');
+}
+
+async function getHeaders(form: FormData){
+    return new Promise((resolve, reject) => {
+        form.getLength((err, length) => {
+            if(err) { reject(err); }
+            let headers = Object.assign({'Content-Length': length}, form.getHeaders());
+            resolve(headers);
+        });
+    });
+}
+
+function getNewId(): string {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + s4() + s4() + s4() + s4();
 }
 
 class ReportalClient{
@@ -16,12 +36,15 @@ class ReportalClient{
     private allSuites: any[] = [];
     private allSpecs: any[] = [];
     private reportalUrl: string;
+    private browser: ProtractorBrowser;
 
-    constructor({projectId, launchName, reportalUrl, disableScreenShots=true}: {projectId: string, launchName: string, reportalUrl: string, disableScreenShots?: boolean}){
+    constructor({projectId, launchName, reportalUrl, browser, disableScreenShots=true}: {projectId: string, launchName: string,
+        reportalUrl: string, disableScreenShots?: boolean, browser: ProtractorBrowser}){
         this.projectId = projectId;
         this.launchName = launchName;
         this.disableScreenShots = disableScreenShots;
         this.reportalUrl = reportalUrl;
+        this.browser = browser;
     }
 
     private getSuiteClone(suite: any) {
@@ -56,7 +79,19 @@ class ReportalClient{
         return this.disableScreenShots;
     }
 
-    async suiteStarted(suite: any) {
+    private async reportScreenShot(screenId: string){
+        const screenShot = await this.browser.takeScreenshot();
+        const form = new FormData();
+        form.append('screen', screenShot);
+        const formHeaders = await getHeaders(form);
+        try {
+            await Axios.post(`http://localhost:3000/report-screen/${screenId}`, form, {headers: formHeaders});
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    suiteStarted(suite: any) {
         try {
             suite = this.getSuiteClone(suite);
             suite.specs = [];
@@ -67,7 +102,7 @@ class ReportalClient{
         }
     };
 
-    async specStarted(spec: any) {
+    specStarted(spec: any) {
         try {
             spec = this.getSpecClone(spec);
             spec.utcStarted = new Date();
@@ -79,29 +114,25 @@ class ReportalClient{
     };
 
     async specDone(spec: any) {
-        try {
-            //immediately take screenshot to keep the state of the browser screen do it async
-            if (!this.skipScreenShooting(spec)){
-                const screenShot = await browser.takeScreenshot();
-            }
-            spec = this.getSpecClone(spec);
-            spec.utcFinished = new Date();
-            spec.duration = (spec.utcFinished - spec.utcStarted)/1000;
+        spec.screenId =getNewId();
 
-            const capabilities =  await browser.getCapabilities();
+        //immediately take screenshot to keep the state of the browser screen do it async
+        if (!this.skipScreenShooting(spec)){
+            this.reportScreenShot(spec.screenId)
+        }
+        spec = this.getSpecClone(spec);
+        spec.utcFinished = new Date();
+        spec.duration = (spec.utcFinished - spec.utcStarted)/1000;
+
+        try {
+            const capabilities =  await this.browser.getCapabilities();
             spec.browserVersion = capabilities.get('version');
             spec.platform = capabilities.get('platform');
             spec.browserName = capabilities.get('browserName');
-            spec.suiteDirectory = path.join(replaceInvalidSymbols(spec.suite), replaceInvalidSymbols(spec.browserName));
             spec.specId = replaceInvalidSymbols(spec.description);
             spec.launchName = this.launchName;
             spec.projectId = this.projectId;
-            //todo axios send request
-            await axios.post(`${this.reportalUrl}/report`, spec);
-
-												
-																  
-			 
+            axios.post(`${this.reportalUrl}/report`, spec).catch(error => console.error(error))
         } catch (e){
             console.error(e);
         }
